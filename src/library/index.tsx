568fcdd71c5f6436ea4a062333b3808c2f2683a5
AbdulazizSulaymon <djshahrick@gmail.com>
2022-04-29 12:00:34 +0500

    new elements

--- src/library/index.tsx
+++ src/library/index.tsx
@@ -1,6 +1,9 @@
 import Box from "./Box/Box.json";
 import Button from "./Button/Button.json";
+import ButtonGroups from "./ButtonGroups/ButtonGroups.json";
 import Image from "./Image/Image.json";
 import Text from "./Text/Text.json";
+import InlineText from "./InlineText/InlineText.json";
+import Card from "./Card/Card.json";
 
-export default [Box, Button, Image, Text];
+export default [Box, Button, ButtonGroups, Image, Text, InlineText, Card];
